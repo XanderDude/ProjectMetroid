99593cbd7423a9aa05bc1a3ec0e9370559a16c6f
XanderDude <ageofeasters@gmail.com>
2025-06-13 16:59:02 -0700

    added godot project

---
+++ Label.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class Label : Godot.Label
+{
+	public override void _Ready()
+	{
+	//    Text = "Hello, World";
+	}
+
+	public override void _Process(double delta)
+	{
+		
+	}
+}
