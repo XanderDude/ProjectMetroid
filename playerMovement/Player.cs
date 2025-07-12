e844be943a36ea3895644415946c5b530a4b2973
XanderDude <ageofeasters@gmail.com>
2025-07-11 18:59:39 -0700

    Added new exciting jungle gym

--- playerMovement/Player.cs
+++ playerMovement/Player.cs
@@ -3,52 +3,102 @@ using System;
 
 public partial class Player : CharacterBody3D 
 {
-    [Export] public float initSpeed = 1.0f;
-    [Export] public float acceleration = 2.0f;
-    [Export] public float maxSpeed = 4.0f;
-    [Export] public float jumpVelocity = 10.5f;
-    [Export] public float gravity = 9.8f;
-    [Export] public float deacceleration = 2.0f;
-    
-    
-    public override void _PhysicsProcess(double delta) 
-    {
-        Vector3 velocity = Velocity;
-        
-        if (!IsOnFloor()) velocity.Y -= gravity * (float)delta;
-        if (!IsOnFloor() && velocity.Y < 6.5f) velocity.Y -= gravity * (float)delta * 5;
-        if (Input.IsKeyPressed(Key.Space) && IsOnFloor())
-        {
-            velocity.Y = jumpVelocity;
-        }
-        if (Input.IsKeyPressed(Key.Left))
-        {
-            if (velocity.X > -maxSpeed) velocity.X -= (initSpeed + acceleration) * (float)delta;
-        }
-        else if (velocity.X < 0) velocity.X += deacceleration * (float)delta * 4;
-        
-
-        if (Input.IsKeyPressed(Key.Right))
-        {
-            if (velocity.X < maxSpeed) velocity.X += (initSpeed + acceleration) * (float)delta;
-        }
-        else if (velocity.X > 0) velocity.X -= deacceleration * (float)delta * 4;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-       
-        
-        
-        
-        
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+   [ExportGroup("Player Movement")]
+	[ExportSubgroup("Grounded State")]
+	[Export] public float groundInitSpeed = 1.0f;
+	[Export] public float groundMaxSpeed = 6.0f;
+	[Export] public float groundAcceleration = 3.0f;
+	[Export] public float groundDeacceleration = 15.0f;
+	
+	private void groundedState(double delta, ref Vector3 velocity) {
+			if (Input.IsKeyPressed(Key.Left)) {
+				if (velocity.X > -groundMaxSpeed) velocity.X -= groundAcceleration * (float)delta; 
+			}
+			else if (Input.IsKeyPressed(Key.Right)) {
+				if (velocity.X <= groundMaxSpeed) velocity.X += groundAcceleration * (float)delta; 
+			}
+			else { 
+		  		if ( velocity.X > 0.1) velocity.X -= groundDeacceleration * (float)delta;
+				else if ( velocity.X < -0.1) velocity.X += groundDeacceleration * (float)delta;
+				else { velocity.X = 0; }
+			}
+		}
+	
+	[ExportSubgroup("Jump State")]
+	[Export] public float jumpInitSpeed = 1.0f;
+	[Export] public float jumpAcceleration = 3.0f;
+	[Export] public float jumpMaxSpeed = 3.0f;
+	[Export] public float jumpVelocity = 5.0f;
+	[Export] public float jumpGravity = 9.8f;
+	[Export] public float jumpMaxHeight = 0.4f;
+	public float jumpHeight = 0.0f;
+	
+	
+	private bool isAscending(double delta, ref Vector3 velocity) {
+		if (Input.IsKeyPressed(Key.Space) && jumpHeight < jumpMaxHeight) { 
+			velocity.Y = jumpVelocity;
+			jumpHeight += (float) delta; 
+			GD.Print("Jump Height: " + jumpHeight);
+			return true; 
+		}
+		else {
+			jumpHeight = jumpMaxHeight;
+			return false;
+		}
+	}
+	
+	private void jumpState(double delta, ref Vector3 velocity) {
+		velocity.Y -= jumpGravity * (float)delta;
+		if (isAscending(delta, ref velocity)) {
+		
+			if (Input.IsKeyPressed(Key.Left)) {
+				velocity.X = -jumpMaxSpeed;
+			}
+			else if (Input.IsKeyPressed(Key.Right)) {
+				velocity.X = jumpMaxSpeed;
+			}
+			else { 
+				if ( velocity.X > 0.1f) velocity.X -= 0.1f;
+				else if ( velocity.X < -0.1f) velocity.X += 0.1f;
+				else { velocity.X = 0; }
+			}
+		}
+		
+		else {
+			
+			if (Input.IsKeyPressed(Key.Left)) {
+				velocity.X = -jumpMaxSpeed / 2;
+			}
+			else if (Input.IsKeyPressed(Key.Right)) {
+				velocity.X = jumpMaxSpeed / 2;
+			}
+			else { 
+				if ( velocity.X > 0.1f) velocity.X = jumpMaxSpeed / 2;
+				else if ( velocity.X < -0.1f) velocity.X = -jumpMaxSpeed / 2;
+				else { velocity.X = 0; }
+			}
+		}
+	
+	}
+	
+	public override void _PhysicsProcess(double delta) {
+		Vector3 velocity = Velocity;
+		GD.Print("Velocity X " + velocity.X);
+		GD.Print("velocity.Y " + velocity.Y);
+	 	
+	if (IsOnFloor()) {
+		groundedState(delta, ref velocity);
+	}
+	else if (!IsOnFloor()) {
+		jumpState(delta, ref velocity);
+	}
+	if (Input.IsKeyPressed(Key.Space) && IsOnFloor()) { 
+		jumpHeight = 0.0f;
+		velocity.Y = jumpVelocity;
+		
+	}
+	
+	Velocity = velocity;
+	MoveAndSlide();
+	}
 }
