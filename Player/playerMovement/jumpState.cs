d0718f7e00f4acc5f1efb2131085e439ebe31be6
Julian <julesnemocraft@gmail.com>
2025-07-21 14:09:32 -0700

    Mantling In Progress

--- Player/playerMovement/jumpState.cs
+++ Player/playerMovement/jumpState.cs
@@ -3,111 +3,135 @@ using System;
 
 public partial class jumpState : State
 {
-    [ExportSubgroup("Jump State")]
-    [Export] public float airMaxSpeed = 5.0f;
+	[ExportSubgroup("Jump State")]
+	[Export] public float airMaxSpeed = 5.0f;
 	[Export] public float jumpAcceleration = 10.0f;
-    [Export] public float jumpDeceleration = 15f;
+	[Export] public float jumpDeceleration = 15f;
  	[Export] public float jumpVelocity = 10.0f;
 	[Export] public float jumpMaxHeight = 0.17f;
+	[Export] public float playerTop = 0.2f;
 	public float jumpHeight = 0.0f;
-    private bool neutralJump = false;
-    private bool IsAscending(float delta, ref Vector3 velocity) //check if player should be ascending
-    {
-        if (jumpHeight < jumpMaxHeight)
-        {
-            velocity.Y = jumpVelocity; //continously set upward velocity
-            jumpHeight += delta;
-            //GD.Print("Jump Height: " + jumpHeight);
-            return true;
-        }
-        else
-        {
-            GD.Print("jump max height reached");
-            jumpHeight = jumpMaxHeight; //clamp
-            return false;
-        }
-    }
+	private bool neutralJump = false;
+	
+	private bool isSameHeight()
+	{
+		if (player.GetSlideCollisionCount() == 0) {
+			GD.Print("No Collision Detected");
+			return false;
+			}
+		KinematicCollision3D collision = player.GetSlideCollision(0);
+		Node3D collider = collision.GetCollider() as Node3D;
+		float playerHeight = player.GlobalPosition.Y + playerTop;
+		float meshTop = collider.GlobalPosition.Y;
+		
+		return Mathf.Abs(playerHeight - meshTop) < 0.1f;
+			
+		
+	}
+	private bool IsAscending(float delta, ref Vector3 velocity) //check if player should be ascending
+	{
+		if (jumpHeight < jumpMaxHeight)
+		{
+			velocity.Y = jumpVelocity; //continously set upward velocity
+			jumpHeight += delta;
+			//GD.Print("Jump Height: " + jumpHeight);
+			return true;
+		}
+		else
+		{
+			GD.Print("jump max height reached");
+			jumpHeight = jumpMaxHeight; //clamp
+			return false;
+		}
+	}
 
-    public override void Enter()
-    {
-        GD.Print("Entered Jump State. Jump queued: " + player.Get("jumpQueued"));
-        if ((bool)player.Get("jumpQueued"))//jump state entered due to player jumping
-        {
-            jumpHeight = 0.0f;
-            //play jump sound
-            if (Input.GetAxis("Left", "Right") == 0) neutralJump = true; //freeze horizontal velocity
-        }
+	public override void Enter()
+	{
+		GD.Print("Entered Jump State. Jump queued: " + player.Get("jumpQueued"));
+		if ((bool)player.Get("jumpQueued"))//jump state entered due to player jumping
+		{
+			jumpHeight = 0.0f;
+			//play jump sound
+			if (Input.GetAxis("Left", "Right") == 0) neutralJump = true; //freeze horizontal velocity
+		}
 
-        player.Set(PlayerManager.PropertyName.slideBoost, true); //player must be airborne, enable boost
-        parentMesh.GetNode<PlayerAnimationHandler>(parentMesh.GetPath()).BeginJump();
-    }
-    public override void Exit()
-    {
-        GD.Print("Exited Jump State");
-        player.Set("jumpQueued", false); //don't jump on exit if holding jump
-        parentMesh.GetNode<PlayerAnimationHandler>(parentMesh.GetPath()).Grounded();
-    }
+		player.Set(PlayerManager.PropertyName.slideBoost, true); //player must be airborne, enable boost
+		parentMesh.GetNode<PlayerAnimationHandler>(parentMesh.GetPath()).BeginJump();
+	}
+	public override void Exit()
+	{
+		GD.Print("Exited Jump State");
+		player.Set("jumpQueued", false); //don't jump on exit if holding jump
+		parentMesh.GetNode<PlayerAnimationHandler>(parentMesh.GetPath()).Grounded();
+	}
 
-    public override void PhysicsUpdate(float delta)
-    {
-        /*if (Input.IsKeyPressed(Key.Down)) {
+	public override void PhysicsUpdate(float delta)
+	{
+		/*if (Input.IsKeyPressed(Key.Down)) {
 				velocity.Y -= (jumpGravity * 4) * (float)delta;
 			}
 		*/
-        HandleAirMovement(delta);
-        player.MoveAndSlide();
-    }
-
-    private void HandleAirMovement(float delta)
-    {
-        Vector3 velocity = player.Velocity;
-        if (neutralJump) velocity.X = 0; neutralJump = false;
-        float input = Input.GetAxis("Left", "Right");
-
-        if ((bool)player.Get("jumpQueued") && IsAscending(delta, ref velocity))
-        { //jump queued set true outside this state. if the player releases jump, the bool is set false 
-            GD.Print("Jumping");
-            velocity.Y -= _gravity * delta;
-            if (input == 0)
-            {
-                velocity.X = Mathf.MoveToward(velocity.X, 0, jumpDeceleration);
-                /*
-                if (velocity.X > 0.3f) velocity.X -= 1.0f;
-                else if (velocity.X < -0.3f) velocity.X += 1.0f;
-                else { velocity.X = 0; }*/
-            }
-            else velocity.X = input * airMaxSpeed;
-
-        }
-        else //must be falling
-        {
-            velocity.Y -= _gravity * 2.5f * delta; //faster falling speed
-            if (input == 0)
-            {
-                velocity.X = Mathf.MoveToward(velocity.X, 0, jumpDeceleration);
-                /*
-                if (velocity.X > 0.1f) velocity.X = 0.5f;
-                else if (velocity.X < -0.1f) velocity.X = -0.5f;
-                */
-            }
-            else velocity.X = input * airMaxSpeed;
-            if (player.IsOnFloor())
-            {
-                if (Input.IsActionPressed("Slide")) msm.TransitionTo("slideState");
-                else msm.TransitionTo("groundedState");
-            }
-        }
-
-        velocity.X = Mathf.Clamp(velocity.X, -airMaxSpeed, airMaxSpeed);//clamp horizontal speed
-        player.Velocity = velocity;
-    }
-
-    public override void HandleInput(InputEvent @event)
-    {
-        if (@event.IsActionReleased("Jump")) //check when jump is released
-        {
-            player.Set(PlayerManager.PropertyName.jumpQueued, false);
-        }
+		player.MoveAndSlide();
+		HandleAirMovement(delta);
+		
 	}
-    
+
+	private void HandleAirMovement(float delta)
+	{
+		Vector3 velocity = player.Velocity;
+		if (neutralJump) velocity.X = 0; neutralJump = false;
+		float input = Input.GetAxis("Left", "Right");
+
+		if ((bool)player.Get("jumpQueued") && IsAscending(delta, ref velocity))
+		{ //jump queued set true outside this state. if the player releases jump, the bool is set false 
+			GD.Print("Jumping");
+			velocity.Y -= _gravity * delta;
+			if (input == 0)
+			{
+				velocity.X = Mathf.MoveToward(velocity.X, 0, jumpDeceleration);
+				/*
+				if (velocity.X > 0.3f) velocity.X -= 1.0f;
+				else if (velocity.X < -0.3f) velocity.X += 1.0f;
+				else { velocity.X = 0; }*/
+			}
+			else velocity.X = input * airMaxSpeed;
+
+		}
+		else //must be falling
+		{
+			velocity.Y -= _gravity * 2.5f * delta; //faster falling speed
+			if (input == 0)
+			{
+				velocity.X = Mathf.MoveToward(velocity.X, 0, jumpDeceleration);
+				/*
+				if (velocity.X > 0.1f) velocity.X = 0.5f;
+				else if (velocity.X < -0.1f) velocity.X = -0.5f;
+				*/
+			}
+			else velocity.X = input * airMaxSpeed;
+			if (isSameHeight()) {
+				GD.Print("Mantling");
+			}
+			if (player.IsOnFloor())
+			{
+				if (Input.IsActionPressed("Slide")) msm.TransitionTo("slideState");
+				else msm.TransitionTo("groundedState");
+			}
+		}
+
+		velocity.X = Mathf.Clamp(velocity.X, -airMaxSpeed, airMaxSpeed);//clamp horizontal speed
+		player.Velocity = velocity;
+	}
+
+	public override void HandleInput(InputEvent @event)
+	{
+		if (@event.IsActionReleased("Jump")) //check when jump is released
+		{
+			player.Set(PlayerManager.PropertyName.jumpQueued, false);
+		}
+	}
+	
+	
+	
+	
 }
--- Player/playerScenes/PlayerManager.cs
+++ Player/playerScenes/PlayerManager.cs
@@ -3,24 +3,24 @@ using System;
 
 public partial class PlayerManager : CharacterBody3D
 {
-    private NodePath playerMesh = "%PlayerMesh"; //unique local node to the Player scene
-    private MovementStateMachine _stateMachine;
+	private NodePath playerMesh = "%PlayerMesh"; //unique local node to the Player scene
+	private MovementStateMachine _stateMachine;
 
 
-    [Export] public bool jumpQueued; //player is holding the jump button
-    [Export] public bool slideQueued; //player is holding slide button
-    [Export] public bool slideBoost; //player is airborn/just landed
-    [Export]
-    private MovementStateMachine StateMachine //init StateMachine
-    {
-        get { return _stateMachine; }
-        set //assign self and mesh to state machine prior to _ready
-        {
-            _stateMachine = value;
-            StateMachine.Parent = this;
-            StateMachine.parentMesh = GetNode<Node3D>(playerMesh);
-        }
-    }
+	[Export] public bool jumpQueued; //player is holding the jump button
+	[Export] public bool slideQueued; //player is holding slide button
+	[Export] public bool slideBoost; //player is airborn/just landed
+	[Export]
+	private MovementStateMachine StateMachine //init StateMachine
+	{
+		get { return _stateMachine; }
+		set //assign self and mesh to state machine prior to _ready
+		{
+			_stateMachine = value;
+			StateMachine.Parent = this;
+			StateMachine.parentMesh = GetNode<Node3D>(playerMesh);
+		}
+	}
 
 
 }
