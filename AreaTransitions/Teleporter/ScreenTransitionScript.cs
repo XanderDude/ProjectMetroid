a32de1f3cd43e650aebc02f60500eb8da184107c
Julian <julesnemocraft@gmail.com>
2025-07-23 14:29:30 -0700

    Teleporter Test
    testing a fade-out transition that deloads a room and loads the next room.

--- AreaTransitions/Teleporter/ScreenTransitionScript.cs
+++ AreaTransitions/Teleporter/ScreenTransitionScript.cs
@@ -4,26 +4,29 @@ using System;
 
 public partial class ScreenTransitionScript : Godot.TextureRect
 {
-	[Export] public string roomPath = "";
+	[Export] public string roomPath;
 	private AnimationPlayer blackScreen;
 	private Area3D collider;
 	private bool isEntering = false;
 	private Node3D player; 
 	private Node3D gameNode;
+	private string currentRoomName;
+	private string lastRoomName;
 	
 	public override void _Ready() {
-		blackScreen = GetNode<AnimationPlayer>("%AnimationPlayer"); 
-		collider = GetNode<Area3D>("%Area3D");
+		blackScreen = GetNode<AnimationPlayer>("/root/Game/GlobalTransition/%AnimationPlayer"); 
+		roomPath =  "Room2";
 		gameNode = GetNode<Node3D>("/root/Game");
 		player = gameNode.GetNode<Node3D>("Player");
 		
-		var currentRoomName = GetParent().GetParent().GetParent().Name; 
-		collider.Monitoring = (currentRoomName == "Room1");
+		currentRoomName = GetCurrentRoom()?.Name.ToString() ?? "";
+		collider = GetNode<Area3D>($"/root/Game/{currentRoomName}/roomTeleportHitbox/%Area3D");
+		if (currentRoomName == "Room1") collider.Monitoring = true;
 	
 }
 	
 	private bool isEnterFromLeft() {
-		return collider.GlobalPosition.X > player.GlobalPosition.X;
+		return collider.GlobalPosition.X >= player.GlobalPosition.X;
 	}
 	
 	private bool isExitFromRight() {
@@ -45,6 +48,8 @@ public partial class ScreenTransitionScript : Godot.TextureRect
 	
 	private void LoadRoom(string roomName) {
 		Modulate = new Color(0, 0, 0, 0);
+		Vector3 teleportOffset =  new Vector3(7, 0, 0);
+		
 		GD.Print("Trying to load room: ", roomName);
 		UnloadCurrentRoom();
 		var newRoom = gameNode.GetNode<Node3D>(roomName);
@@ -52,13 +57,13 @@ public partial class ScreenTransitionScript : Godot.TextureRect
 			GD.Print("Successfully found room: ", roomName);
 			newRoom.Visible = true;
 			newRoom.ProcessMode = Node.ProcessModeEnum.Inherit;
-		
-			var targetTeleporter = newRoom.GetNode<Node3D>("roomTeleportHitbox");
-				player.GlobalPosition = targetTeleporter.GlobalPosition + new Vector3(7, 15, 0);
-				
-		
-			var newRoomArea = newRoom.GetNode<Area3D>("roomTeleportHitbox/Area3D");
-			newRoomArea.Monitoring = true;
+			
+			var newRoomTeleporter = newRoom.GetNode<Area3D>("roomTeleportHitbox/Area3D");
+				GD.Print("entering from left");
+				player.GlobalPosition = newRoomTeleporter.GlobalPosition;
+			
+			 
+			newRoomTeleporter.Monitoring = true;
 	} 
 	else { 
 			GD.Print("Failed to find room: ", roomName);
@@ -68,7 +73,7 @@ public partial class ScreenTransitionScript : Godot.TextureRect
 	
 	private Node3D GetCurrentRoom() {
 		foreach (Node3D child in gameNode.GetChildren()) {
-			if (child.Visible) {
+			if (child.Visible && child.Name.ToString().StartsWith("Room")) {
 				return child;
 			}
 		}
@@ -76,29 +81,31 @@ public partial class ScreenTransitionScript : Godot.TextureRect
 	}
 	
 	public void _on_area_3d_body_entered(Node3D body) {
-		
 	if (body == player && !isEntering && !string.IsNullOrEmpty(roomPath)) {
 		isEntering = true;
-		
+		lastRoomName = GetCurrentRoom()?.Name.ToString() ?? "";
+		GD.Print("I Entered from room: ", lastRoomName);
 		blackScreen.Play("fade_in_black_screen");
 		GetTree().CreateTimer(1.1f).Timeout += () => {
-		blackScreen.Play("fade_out_black_screen");
-		GD.Print("Playing Fade Out");
-		GD.Print("Loading Room");
 		LoadRoom(roomPath);
-		GD.Print("Setting Modulate to 0");
-		
-		GD.Print("Getting Camera Node");
+		blackScreen.Play("fade_out_black_screen");
 		var camera = gameNode.GetNode<Camera3D>("Camera3D") as Camera3d;
 		camera.Position = new Vector3(player.Position.X, player.Position.Y + camera.cameraYOffset, 25.0f);
 		};
 		
+		
+		
 	}
 }
 	public void _on_area_3d_body_exited(Node3D body) {
-		GD.Print("I Exited");
-		isEntering = false;
-		blackScreen.ClearQueue();
+			GD.Print("roomPath: ", roomPath);
+		 	currentRoomName = GetCurrentRoom()?.Name.ToString() ?? "";
+		 	GD.Print("I Exited from current room: ", currentRoomName);
+			if (currentRoomName != lastRoomName) {
+				blackScreen.ClearQueue();
+				isEntering = false;
+				roomPath = lastRoomName;
+			}
 		
 	}
 		
