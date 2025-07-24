2da862ca123d8998b9dd1459335dcc207bbf96c5
XanderDude <ageofeasters@gmail.com>
2025-07-23 18:51:12 -0700

    spike trap, started pausing functionality

--- Player/playerScenes/PlayerManager.cs
+++ Player/playerScenes/PlayerManager.cs
@@ -11,6 +11,36 @@ public partial class PlayerManager : CharacterBody3D
 	public bool slideQueued; //player is holding slide button
 	public bool slideBoost; //player is airborn/just landed
 
+    private HUD hud;
+    private int health = 100;
+
+    private bool gamePaused;
+    public bool GamePaused
+    {
+        get { return gamePaused; }
+        set
+        {
+            gamePaused = value;
+            GetTree().Paused = value;
+        }
+    }
+
+    public int Health //gets current health of the player, sets new health value and updates UI
+    {
+        get { return health; }
+        set
+        {
+            health = value;
+            if (health <= 0)
+            {
+                health = 0;
+                hud.PlayerDied();
+                GamePaused = true;
+            }
+            hud.UpdateHealthBar(Health);
+        }
+    }
+
 	[Export]
 	private MovementStateMachine StateMachine //init StateMachine
 	{
@@ -29,5 +59,7 @@ public partial class PlayerManager : CharacterBody3D
 	public override void _Ready()
 	{
 		
+        hud = (HUD)GetTree().GetFirstNodeInGroup("hud");
+        hud.UpdateHealthBar(Health);
 	}
 }
