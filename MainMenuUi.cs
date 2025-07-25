a4972d41f99c658eccd59eb47b6a9701edc9a22f
Julian <julesnemocraft@gmail.com>
2025-07-24 21:01:50 -0700

    Main Menu UI
    -Implemented an animated background, customized buttons

--- MainMenuUi.cs
+++ MainMenuUi.cs
@@ -3,6 +3,15 @@ using System;
 
 public partial class MainMenuUi : CanvasLayer
 {
+[Export] Godot.AudioStreamPlayer titleMP3;
+AnimationPlayer fogAnimation;
+
+public override void _Ready() {
+	titleMP3 = GetNode<Godot.AudioStreamPlayer>("%titleMusic");
+	fogAnimation = GetNode<AnimationPlayer>("%titleFog");
+	fogAnimation.Play("fog_animation");
+	titleMP3.Play();
+}
 	
 public void _on_play_pressed() {
 	GetTree().ChangeSceneToFile("res://Environment/Scenes/TexturesAndLighting.tscn");
