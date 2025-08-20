29face4773f177120089fee7fcf269c0d1f1ae1f
Julian <julesnemocraft@gmail.com>
2025-08-19 22:51:03 -0700

    Raven Recall and Launch

--- Player/Weapons/Raven.cs
+++ Player/Weapons/Raven.cs
@@ -5,15 +5,20 @@ public partial class Raven : CharacterBody3D
     [Export] PlayerManager player;
     [Export] public float speed;
     [Export] public float maxDistance;
-    public bool isAbility = false;
 
+    public Vector3 direction = Vector3.Zero;
+    public Vector3 targetPosition = Vector3.Zero;
+    public bool isInAction = false;
+    public bool isLaunched = false;
+
+   
     public override void _Ready()
     {
 
         //GD.Print("Raven: Loaded successfully");
         if (player == null)
         {
-            //GD.PrintErr("Raven: player not found");
+            GD.PrintErr("Raven: player not found");
         }
     }
 
@@ -21,61 +26,106 @@ public partial class Raven : CharacterBody3D
 
     public override void _PhysicsProcess(double delta)
     {
-        if (!isAbility)
+        if (!isInAction)
         {
             Vector3 Yoffset = new Vector3(0, 1.5f, 0);
             Vector3 direction = GlobalPosition.DirectionTo(player.GlobalPosition + Yoffset);
 
             Velocity = (direction) * speed;
-            MoveAndSlide();
+
+             if (Mathf.Abs(GlobalPosition.DistanceTo(player.GlobalPosition + Yoffset)) < 0.1f) { isLaunched = false; }
         }
+
+        if (Input.IsActionPressed("RavenTest") && !isLaunched)
+        {
+            RavenLaunch();
+        }
+        if (Input.IsActionJustReleased("RavenTest"))
+        {
+            isLaunched = true;
+            Velocity = Vector3.Zero;
+        }
+
+        if (Input.IsActionJustPressed("RavenTest") && isLaunched)
+        {
+            isInAction = false;
+           
+        }
+
+
+
+            MoveAndSlide();
     }
 
-    public Vector2 RavenDirection()
+    public Vector3 RavenDirection()
     {
-        Vector2 dir = Vector2.Zero;
+        Vector3 dir = Vector3.Zero;
         if (Input.IsActionPressed("Up")) dir.Y += 1;
         if (Input.IsActionPressed("Down")) dir.Y -= 1;
         if (Input.IsActionPressed("Left")) dir.X -= 1;
         if (Input.IsActionPressed("Right")) dir.X += 1;
 
-        if (dir != Vector2.Zero)
-            return new Vector2(Input.GetAxis("Left", "Right"), Input.GetAxis("Up", "Down")).Normalized();
+        if (dir != Vector3.Zero)
+            return new Vector3(Input.GetAxis("Left", "Right"), Input.GetAxis("Down", "Up"), 0).Normalized();
 
-        return Vector2.Zero;
+        return Vector3.Zero;
 
     }
+    
+
+
 
     public void RavenLaunch()
     {
+
+
         if (player == null)
         {
-           // GD.PrintErr("Raven: PlayerManager is null");
+            GD.PrintErr("Raven: PlayerManager is null");
             return;
         }
 
-        if (isAbility) return;
+       
+        if (isInAction == false)
+        {
+            direction = RavenDirection();
+            targetPosition = GlobalPosition + (direction * maxDistance);
+        }
 
-        isAbility = true;
-        Vector2 direction = RavenDirection();
-        var currentPosition = GlobalPosition;
-
-
-        if (direction != Vector2.Zero)
+        isInAction = true;
+        if (direction != Vector3.Zero)
         {
 
-           //GD.Print("Raven: Launched in Direction:  " + direction);
+            GD.Print("Raven: Launched in Direction:  " + direction);
+            Velocity = direction * speed;
+
+            if (Mathf.Abs(GlobalPosition.DistanceTo(targetPosition)) < 0.1f)
+            {
+                GD.Print("Raven: Reached target position");
+                GD.Print("Raven: targetPosition: " + targetPosition);
+                Velocity = Vector3.Zero;
+            }
+            else
+            {
+                GD.Print("Raven: Moving towards target position");
+                GD.Print("Raven: Current Position: " + GlobalPosition);
+                GD.Print("Raven: targetPosition: " + targetPosition);
+            }
+
+
         }
-        else
-        {
-           // GD.Print("Raven: Direction is zero");
-        }
+
+
 
 
     }
 
+    public void RavenRecall()
+    {
+        
+    }
 
-    public void RavenRecall() { }
+   
     public void RavenTeleport() { }
     public void RavenMeleeAttack() { }
 
