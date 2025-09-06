a425c9a77502472bd40c32ee8f08b9bb343d2081
Julian Nemo <julesnemocraft@gmail.com>
2025-09-05 20:41:48 -0700

    Raven Camera Movement
    -Raven follows the player slowly

--- Raven/RavenLaunchState.cs
+++ Raven/RavenLaunchState.cs
@@ -3,79 +3,80 @@ using Godot;
 
 public partial class RavenLaunchState : State
 {
-    public override void Enter()
-    {
-        //GD.Print("Raven: Entered Launch State");
+	public override void Enter()
+	{
+		//GD.Print("Raven: Entered Launch State");
 
-    }
-    public override void Exit()
-    {
-    }
-    public override void Update(float delta)
-    {
-    }
-    public override void PhysicsUpdate(float delta)
-    {
-        RavenLaunch();
-    }
-    public override void HandleInput(InputEvent @event)
-    {
-        if (@event.IsActionReleased("RavenSpecial"))
-        {
-            rsm.TransitionTo("RavenIdleState");
-        }
-    }
+	}
+	public override void Exit()
+	{
+	}
+	public override void Update(float delta)
+	{
+	}
+	public override void PhysicsUpdate(float delta)
+	{
+		RavenLaunch();
+	}
+	public override void HandleInput(InputEvent @event)
+	{
+		if (@event.IsActionReleased("RavenSpecial"))
+		{
+			rsm.TransitionTo("RavenIdleState");
+		}
+	}
 
 
 
-    public Vector3 RavenDirection()
-    {
-        Vector3 dir = Vector3.Zero;
-        if (Input.IsActionPressed("Up")) dir.Y += 1;
-        if (Input.IsActionPressed("Down")) dir.Y -= 1;
-        if (Input.IsActionPressed("Left")) dir.X -= 1;
-        if (Input.IsActionPressed("Right")) dir.X += 1;
-        if (dir != Vector3.Zero)
-            return new Vector3(Input.GetAxis("Left", "Right"), Input.GetAxis("Down", "Up"), 0).Normalized();
+	public Vector3 RavenDirection()
+	{
+		Vector3 dir = Vector3.Zero;
+		if (Input.IsActionPressed("Up")) dir.Y += 1;
+		if (Input.IsActionPressed("Down")) dir.Y -= 1;
+		if (Input.IsActionPressed("Left")) dir.X -= 1;
+		if (Input.IsActionPressed("Right")) dir.X += 1;
+		if (dir != Vector3.Zero)
+			return new Vector3(Input.GetAxis("Left", "Right"), Input.GetAxis("Down", "Up"), 0).Normalized();
 
-        else
-            return new Vector3(Mathf.Sign(raven.player.GetNode<Node3D>("%PlayerMesh").RotationDegrees.Y), 0, 0);  
-    }
+		else
+			return new Vector3(Mathf.Sign(raven.player.GetNode<Node3D>("%PlayerMesh").RotationDegrees.Y), 0, 0);  
+	}
 
-    public void RavenLaunch()
-    {
-        if (raven.player == null)
-        {
-            GD.PrintErr("Raven: PlayerManager is null");
-            return;
-        }
-        if (!raven.isInAction)
-        {
-            raven.direction = RavenDirection();
-            raven.targetPosition = raven.GlobalPosition + (raven.direction * raven.maxDistance);
-            raven.Velocity = raven.direction * raven.speed;
-        }
-        raven.isInAction = true;
-        if (raven.direction != Vector3.Zero)
-        {
-           
-            
-           // GD.Print("Raven: Launched in Direction:  " + raven.direction);
-            if (Mathf.Abs(raven.GlobalPosition.DistanceTo(raven.targetPosition)) < 0.1f)
-            {
-                
-                //GD.Print("Raven: Reached target position");
-                //GD.Print("Raven: targetPosition: " + raven.targetPosition);
-                raven.Velocity = Vector3.Zero;
-            }
-            else
-            {
+	public void RavenLaunch()
+	{
+		if (raven.player == null)
+		{
+			GD.PrintErr("Raven: PlayerManager is null");
+			return;
+		}
+		if (!raven.isInAction)
+		{
+			raven.direction = RavenDirection();
+			raven.targetPosition = raven.GlobalPosition + (raven.direction * raven.maxDistance);
+			raven.Velocity = raven.direction * raven.speed;
+		}
+		raven.isInAction = true;
+		if (raven.direction != Vector3.Zero)
+		{
+		   
+			
+		   // GD.Print("Raven: Launched in Direction:  " + raven.direction);
+			if (Mathf.Abs(raven.GlobalPosition.DistanceTo(raven.targetPosition)) < 0.1f)
+			{
+				
+				//GD.Print("Raven: Reached target position");
+				//GD.Print("Raven: targetPosition: " + raven.targetPosition);
+				raven.Velocity = Vector3.Zero;
+				rsm.TransitionTo("RavenIdleState");
+			}
+			else
+			{
 
-              // GD.Print("Raven: Moving towards target position");
-                //GD.Print("Raven: Current Position: " + raven.GlobalPosition);
-                //GD.Print("Raven: targetPosition: " + raven.targetPosition);
-               
-            }
-        }
-    }
+			  // GD.Print("Raven: Moving towards target position");
+				//GD.Print("Raven: Current Position: " + raven.GlobalPosition);
+				//GD.Print("Raven: targetPosition: " + raven.targetPosition);
+			   
+			}
+		}
+	}
 }
--- Raven/RavenOnPlayerState.cs
+++ Raven/RavenOnPlayerState.cs
@@ -3,50 +3,68 @@ using System;
 
 public partial class RavenOnPlayerState : State
 {
+	private float followSpeed = 3.0f;
+	private float bobAmount = 0.3f;
+	private float bobSpeed = 2.0f;
+	private float timeAccumulator = 0.0f;
+	
 	public override void Enter()
 	{
-		//GD.Print("Raven: Entered OnPlayer State");
 		raven.isInAction = false;
 		raven.isOnPlayer = true;
 	}
-
+	
 	public override void Exit()
 	{
-		//GD.Print("Raven: Exited OnPlayer State");
 		raven.isOnPlayer = false;
 	}
-
+	
 	public override void Update(float delta)
 	{
-		// Handle player movement direction for raven positioning
+		timeAccumulator += delta;
+		
 		if (Input.GetAxis("Left", "Right") == 1)
 		{
-			raven.Xoffset = new Vector3(0.5f, 0, 0);
+			raven.Xoffset = new Vector3(-1.0f, 0, 0);
 			raven.RotationDegrees = new Vector3(0, 180, 0);
 		}
 		else if (Input.GetAxis("Left", "Right") == -1)
 		{
-			raven.Xoffset = new Vector3(-0.5f, 0, 0);
+			raven.Xoffset = new Vector3(1.0f, 0, 0);
 			raven.RotationDegrees = new Vector3(0, 0, 0);
 		}
 	}
-
+	
 	public override void PhysicsUpdate(float delta)
 	{
-		// Stick the raven to the player's position with offsets
-		Vector3 targetPosition = raven.player.GlobalPosition + raven.Yoffset + raven.Xoffset;
-		raven.GlobalPosition = targetPosition;
-		raven.Velocity = Vector3.Zero; // No independent movement while on player
+		Vector3 baseTarget = raven.player.GlobalPosition + raven.Yoffset + raven.Xoffset;
+		
+		float bobOffset = Mathf.Sin(timeAccumulator * bobSpeed) * bobAmount;
+		Vector3 targetPosition = baseTarget + new Vector3(0, bobOffset, 0);
+		
+		raven.direction = raven.GlobalPosition.DirectionTo(targetPosition);
+		float distanceToTarget = raven.GlobalPosition.DistanceTo(targetPosition);
+		
+		if (distanceToTarget > 0.2f)
+		{
+			raven.Velocity = raven.direction * followSpeed;
+		}
+		else
+		{
+			raven.Velocity = raven.Velocity.MoveToward(Vector3.Zero, followSpeed * delta * 3);
+		}
 	}
-
+	
 	public override void HandleInput(InputEvent @event)
 	{
 		if (@event.IsActionPressed("RavenSpecial"))
 		{
+			raven.GlobalPosition = raven.player.GlobalPosition + raven.Yoffset;
 			rsm.TransitionTo("RavenLaunchState");
 		}
 		else if (@event.IsActionPressed("RavenSlash"))
 		{
+			raven.GlobalPosition = raven.player.GlobalPosition + raven.Yoffset;
 			rsm.TransitionTo("RavenAttackState");
 		}
 	}
