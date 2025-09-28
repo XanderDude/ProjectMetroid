97d79a821a64e8deaefd14bb7950450e2061be9e
XanderDude <ageofeasters@gmail.com>
2025-09-27 18:37:58 -0700

    started raven animation tree

---
+++ Raven/RavenAnimHandler.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+
+public partial class RavenAnimHandler : AnimationTree
+{
+	[Export] private RavenStateMachine rsm;
+	private AnimationNodeStateMachinePlayback playback;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		playback = (AnimationNodeStateMachinePlayback)Get("parameters/playback");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (rsm._currentState.Name == "RavenLaunchState")
+		{
+			playback?.Travel("Launching");
+		}
+		else if (rsm._currentState.Name == "RavenIdleState")
+		{
+			playback?.Travel("Flying");
+		}
+		else playback?.Travel("Hovering");
+	}
+}
