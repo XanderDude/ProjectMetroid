5529102990b3252e9647a98464e933e30b414791
Julian <julesnemocraft@gmail.com>
2026-07-17 16:58:44 -0700

    Up to date

--- Enemies/Traps/SimpleSpike.cs
+++ Enemies/Traps/SimpleSpike.cs
@@ -6,41 +6,34 @@ public partial class SimpleSpike : Node3D
 
 	[Export] private int damage = 5;
 	[Export] private float trapKnockback = 2f;
-	private PlayerManager player;
 
-	private bool playerInRange = false;
-
-	// Called when the node enters the scene tree for the first time.
-
+	private PlayerManager playerInRange;
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{ 
-        if (playerInRange && GameFriend.gameinstance.player.canBeDamaged) 
-		{
-			GD.Print("Player is touching spike");
-
-			DamagePlayer(GameFriend.gameinstance.player);
-		}
+	{
+        if (playerInRange != null && playerInRange.canBeDamaged)
+			DamagePlayer(playerInRange);
 	}
 
 	public void OnLeaveTrapTrigger(Node3D node)
     {
-        playerInRange = false;
+        if (node is PlayerManager p && p == playerInRange)
+            playerInRange = null;
     }
 
     public void OnTrapTriggered(Node3D node)
     {
-		//AssignPlayer();
-        playerInRange = true;
+        if (node is PlayerManager p)
+            playerInRange = p;
     }
 
 	public void DamagePlayer(PlayerManager p)
 	{
 		float knockDir = Mathf.Sign(p.GlobalPosition.X - GlobalPosition.X);
 		p.knockbackVelocity = new Vector3(knockDir, 1f, 0f).Normalized() * trapKnockback * 3f;
-		p.StateMachine.TransitionTo("knockbackState");
-		p.Health -= damage;
+		p.psm.transition_to("knockbackState");
+		p.health -= damage;
 	}
 
 }
